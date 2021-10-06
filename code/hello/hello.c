1cbf5f50f8ca8f9ee9d955c672d4eccd2ad17cab
helinfeng <753280213@qq.com>
2021-10-06 14:06:11 +0800

    add hello

--- code/hello/hello.c
+++ code/hello/hello.c
@@ -1,129 +1,280 @@
-#include<linux/init.h>
-#include<linux/module.h>
-#include<linux/fs.h>
-#include<linux/cdev.h>
-#include<linux/device.h>
-#include"hello.h"
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/slab.h> // kmalloc
+#include <linux/proc_fs.h>
+#include <linux/device.h>
+#include "hello.h"
 
-module_param(major,int,S_IRUGO);
-module_param(minor,int,S_IRUGO);
+static dev_t hello_devno;     /*设备编号*/
+static int hello_major = 232; /*主设备号*/
+static int hello_minor = 0;   /*从设备号*/
 
-static int hello_dev_open(struct inode *inode, struct file *file)
+static struct class *hello_class;  /*sysfs结构*/
+static struct char_dev *hello_dev; /*字符设备*/
+
+module_param(hello_major, int, S_IRUGO);
+module_param(hello_minor, int, S_IRUGO);
+
+static int hello_open(struct inode *inode, struct file *filp)
 {
-    try_module_get(THIS_MODULE);
-    printk(KERN_INFO HELLO_DEVICE_NODE_NAME " opened!\n");
-    return 0;
+    struct char_dev *dev;
+    if (try_module_get(THIS_MODULE))
+    {
+        dev = container_of(inode->i_cdev, struct char_dev, dev);
+        filp->private_data = dev;
+        printk(KERN_INFO HELLO_DEVICE_NODE_NAME " open successful!\n");
+        return 0;
+    }
+    else
+    {
+        printk(KERN_INFO HELLO_DEVICE_NODE_NAME " open failed!\n");
+        return -1;
+    }
 }
 
-static int hello_dev_release(struct inode *inode, struct file *file)
-{ 
-    printk(KERN_INFO HELLO_DEVICE_NODE_NAME " closed!\n");
+static int hello_release(struct inode *inode, struct file *filp)
+{
+    printk(KERN_INFO HELLO_DEVICE_NODE_NAME " close successful!\n");
     module_put(THIS_MODULE);
     return 0;
 }
 
-static ssize_t hello_dev_read(struct file *file, char *buf, size_t count, loff_t *f_pos)
+static ssize_t hello_read(struct file *filp, char *buf, size_t count, loff_t *f_pos)
 {
+    ssize_t err = 0;
+    struct char_dev *dev = filp->private_data;
+
+    if (down_interruptible(&(dev->sem)))
+    {
+        return -ERESTARTSYS;
+    }
+
+    if (count < sizeof(dev->val))
+    {
+        goto out;
+    }
+
     printk(KERN_INFO HELLO_DEVICE_NODE_NAME " read method!\n");
-    return count;
+
+    err = sizeof(dev->val);
+
+out:
+    up(&(dev->sem));
+    return err;
 }
 
-static ssize_t hello_dev_write(struct file *file, const char *buf, size_t count, loff_t *f_pos)
+static ssize_t hello_write(struct file *filp, const char *buf, size_t count, loff_t *f_pos)
 {
+    ssize_t err = 0;
+    struct char_dev *dev = filp->private_data;
+
+    if (down_interruptible(&(dev->sem)))
+    {
+        return -ERESTARTSYS;
+    }
+
+    if (count != sizeof(dev->val))
+    {
+        goto out;
+    }
+
+    if (copy_from_user(&(dev->val), buf, count))
+    {
+        err = -EFAULT;
+        goto out;
+    }
+
     printk(KERN_INFO HELLO_DEVICE_NODE_NAME " write method!\n");
+
+    err = sizeof(dev->val);
+out:
+    up(&(dev->sem));
+    return err;
+}
+
+static ssize_t __hello_get_val(struct char_dev *dev, char *buf)
+{
+    int val = 0;
+
+    if (down_interruptible(&(dev->sem)))
+    {
+        return -ERESTARTSYS;
+    }
+
+    val = dev->val;
+    up(&(dev->sem));
+
+    return snprintf(buf, PAGE_SIZE, "%d\n", val);
+}
+
+static ssize_t __hello_set_val(struct char_dev *dev, const char *buf, size_t count)
+{
+    int val = 0;
+    val = simple_strtol(buf, NULL, 10);
+    if (down_interruptible(&(dev->sem)))
+    {
+        return -ERESTARTSYS;
+    }
+
+    dev->val = val;
+    up(&(dev->sem));
+
     return count;
 }
 
-static long hello_dev_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+static ssize_t hello_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
-    printk(KERN_INFO HELLO_DEVICE_NODE_NAME " ioctl method!\n");
+    struct char_dev *hdev = (struct char_dev *)dev_get_drvdata(dev);
+    return __hello_get_val(hdev, buf);
+}
+
+static ssize_t hello_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
+{
+    struct char_dev *hdev = (struct char_dev *)dev_get_drvdata(dev);
+    return __hello_set_val(hdev, buf, count);
+}
+
+static DEVICE_ATTR(hello, S_IRUGO | S_IWUSR, hello_show, hello_store);
+
+struct file_operations hello_fops = {
+    .owner = THIS_MODULE,
+    .read = hello_read,
+    .write = hello_write,
+    .open = hello_open,
+    .release = hello_release,
+};
+
+static int __hello_setup_dev(struct char_dev *dev)
+{
+    int err;
+
+    memset(dev, 0, sizeof(struct char_dev));
+
+    cdev_init(&(dev->dev), &hello_fops);
+    dev->dev.owner = THIS_MODULE;
+
+    err = cdev_add(&(dev->dev), hello_devno, 1);
+    if (err)
+    {
+        return err;
+    }
+
+    sema_init(&(dev->sem), 1);
+    dev->val = 0;
+
     return 0;
 }
 
-struct file_operations hello_cdev_fops = {
-    .owner = THIS_MODULE,
-    .read = hello_dev_read,
-    .write = hello_dev_write,
-    .open = hello_dev_open,
-    .release = hello_dev_release,
-    .unlocked_ioctl = hello_dev_ioctl
-};
-
-static int __init hello_dev_init(void)
+static int __init hello_init(void)
 {
-    int ret;
-    
+    int err = -1;
+    struct device *temp = NULL;
     /*获取设备编号*/
-    if(major>0)
+    if (hello_major > 0)
     {
         /*静态获取设备号*/
-        devno = MKDEV(major,minor);
-        ret = register_chrdev_region(devno,1,HELLO_DEVICE_NODE_NAME);
+        hello_devno = MKDEV(hello_major, hello_minor);
+        err = register_chrdev_region(hello_devno, 1, HELLO_DEVICE_NODE_NAME);
     }
     else
     {
         /*动态获取设备号*/
-        devno = MKDEV(major,minor);
-        ret = alloc_chrdev_region(&devno, minor, 1, HELLO_DEVICE_NODE_NAME);
-        major = MAJOR(devno);
+        hello_devno = MKDEV(hello_major, hello_minor);
+        err = alloc_chrdev_region(&hello_devno, hello_minor, 1, HELLO_DEVICE_NODE_NAME);
+        hello_major = MAJOR(hello_devno);
+        hello_minor = MINOR(hello_minor);
     }
-    if(ret<0)
+    if (err < 0)
     {
-        printk(KERN_ERR HELLO_DEVICE_NODE_NAME " cannot get major %d \n", major);
-        return -1;
-    }
-
-    printk(KERN_ERR HELLO_DEVICE_NODE_NAME " get major %d \n", major);
-
-    /*注册设备*/
-    hello_dev = cdev_alloc();
-    if(hello_dev!=NULL)
-    {
-        /*初始化设备*/
-        cdev_init(hello_dev,&hello_cdev_fops);
-        hello_dev->owner = THIS_MODULE;
-        /*添加设备*/
-        if(cdev_add(hello_dev,devno,1)!=0)
-        {
-            printk(KERN_ERR HELLO_DEVICE_NODE_NAME " add cdev error!\n");
-            goto error;
-        }
-    }
-    else
-    {
-        printk(KERN_ERR HELLO_DEVICE_NODE_NAME " cdev_alloc!\n");
-        return -1;
+        printk(KERN_ALERT "Failed to alloc hello_dev region.\n");
+        goto fail;
     }
 
-    /*在/sys/class/下创建hello_dev_class目录*/
-    hello_dev_class = class_create(THIS_MODULE,HELLO_DEVICE_CLASS_NAME);
-    if(IS_ERR(hello_dev_class))
+    hello_dev = kmalloc(sizeof(struct char_dev), GFP_KERNEL);
+    if (!hello_dev)
     {
-        printk(KERN_INFO HELLO_DEVICE_NODE_NAME " create class error\n");
-        return -1;
+        err = -ENOMEM;
+        printk(KERN_ALERT "Failed to alloc hell_dev.\n");
+        goto unregister;
+    }
+
+    err = __hello_setup_dev(hello_dev);
+    if (err)
+    {
+        printk(KERN_ALERT "Failed to setup dev: %d.\n", err);
+        goto cleanup;
+    }
+
+    /*在/sys/class/下创建hello目录*/
+    hello_class = class_create(THIS_MODULE, HELLO_DEVICE_CLASS_NAME);
+    if (IS_ERR(hello_class))
+    {
+        err = PTR_ERR(hello_class);
+        printk(KERN_ALERT "Failed to create hello class.\n");
+        goto destroy_cdev;
     }
 
     /*在/dev下创建hello_dev文件*/
-    device_create(hello_dev_class,NULL,devno,NULL,HELLO_DEVICE_FILE_NAME);
+    temp = device_create(hello_class, NULL, hello_devno, NULL, HELLO_DEVICE_FILE_NAME);
+    if (IS_ERR(temp))
+    {
+        err = PTR_ERR(temp);
+        printk(KERN_ALERT "Failed to create hello device.\n");
+        goto destroy_class;
+    }
+
+    err = device_create_file(temp, &dev_attr_hello);
+    if (err < 0)
+    {
+        printk(KERN_ALERT "Failed to create attribute val.\n");
+        goto destroy_device;
+    }
+
+    printk(KERN_ALERT "Successed to initialize hello device.\n");
 
     return 0;
 
-    error:
-        /*释放已经获得的设备号*/
-        unregister_chrdev_region(devno, 1);
-        return ret;
+destroy_device:
+    device_destroy(hello_class, hello_devno);
+
+destroy_class:
+    class_destroy(hello_class);
+
+destroy_cdev:
+    cdev_del(&(hello_dev->dev));
+
+cleanup:
+    kfree(hello_dev);
+
+unregister:
+    /*释放已经获得的设备号*/
+    unregister_chrdev_region(hello_devno, 1);
+
+fail:
+    return err;
 }
 
-static void __exit hello_dev_exit(void)
+static void __exit hello_exit(void)
 {
-    printk(KERN_INFO HELLO_DEVICE_NODE_NAME " dev exit\n");
-    device_destroy(hello_dev_class,devno);
-    class_destroy(hello_dev_class);
-    cdev_del(hello_dev);    /*移除设备*/
-    unregister_chrdev_region(devno,1); /*释放设备号*/
+    printk(KERN_ALERT "Destroy hello device.\n");
+
+    if (hello_class)
+    {
+        device_destroy(hello_class, hello_devno);
+        class_destroy(hello_class);
+    }
+
+    if (hello_dev)
+    {
+        cdev_del(&(hello_dev->dev));
+        kfree(hello_dev);
+    }
+
+    unregister_chrdev_region(hello_devno, 1); /*释放设备号*/
 }
 
-module_init(hello_dev_init);
-module_exit(hello_dev_exit);
+module_init(hello_init);
+module_exit(hello_exit);
 
 MODULE_LICENSE("GPL");
--- code/hello/hello.h
+++ code/hello/hello.h
@@ -1,5 +1,5 @@
-#ifndef _HELLO_ANDROID_H_
-#define _HELLO_ANDROID_H_
+#ifndef _HELLO_H_
+#define _HELLO_H_
 
 #include <linux/cdev.h>
 #include <linux/semaphore.h>
@@ -9,14 +9,10 @@
 #define HELLO_DEVICE_PROC_NAME "hello"
 #define HELLO_DEVICE_CLASS_NAME "hello"
 
-static int major = 232;
-static int minor = 0;
-
-struct cdev *hello_dev; /*cdev数据结构*/
-static dev_t devno; /*设备编号*/
-static struct class *hello_dev_class;
-
-
+struct char_dev
+{
+    int val;              /*寄存器*/
+    struct cdev dev;     /*cdev数据结构*/
+    struct semaphore sem; /*信号量*/
+};
 #endif
-
-
